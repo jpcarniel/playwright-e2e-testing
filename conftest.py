1433d35ef3fccc4b10ada345de5020f6bfcf07a5
João Paulo Carniel Fonseca <carnielfonseca@gmail.com>
2026-04-11 00:49:45 -0300

    style: remover parênteses desnecessários dos fixtures

--- conftest.py
+++ conftest.py
@@ -11,46 +11,46 @@ from pages.key_presses_page import KeyPressesPage
 from pages.login_page import LoginPage
 
 
-@pytest.fixture()
+@pytest.fixture
 def checkboxes_page(page):
     return CheckboxesPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def drag_and_drop_page(page):
     return DragAndDropPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def dropdown_page(page):
     return DropdownPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def dynamic_loading_page(page):
     return DynamicLoadingPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def file_upload_page(page):
     return FileUploadPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def hovers_page(page):
     return HoversPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def javascript_alerts_page(page):
     return JavaScriptAlertsPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def key_presses_page(page):
     return KeyPressesPage(page)
 
 
-@pytest.fixture()
+@pytest.fixture
 def login_page(page):
     return LoginPage(page)
